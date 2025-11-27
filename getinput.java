dc7be035c233d5a0876ed2bce74fd02c461621f5
manish-cloud-n <manishengr20@gmail.com>
2025-11-27 20:43:57 +0530

    getinput

---
+++ getinput.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class getinput {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int c = obj.nextInt();
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a);
+    }
+}
